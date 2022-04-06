75651a468e4142888512accb458892139e48d44f
HASNA BSK <hasnabouisskrane@gmail.com>
2022-04-06 11:00:32 +0200

    Initial Commit

---
+++ src/main/java/metier/MetierImpl.java
@@ -0,0 +1,28 @@
+package metier;
+
+import dao.IDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("metier")
+
+public class MetierImpl implements IMetier {
+   //couplage faible
+    @Autowired
+    @Qualifier("dao2")
+    private IDao dao;
+    public MetierImpl(IDao dao){
+        this.dao=dao;
+    }
+    @Override
+    public double calcul() {
+        double tmp=dao.getData();
+        double res=tmp*540/Math.cos(tmp*Math.PI);
+        return res;
+    }
+// Injecter dans la variable un objet d'une classe qui implente l'interface IDao
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+}
